f03a09b792dcb204d30202a0396b0d64f60f3ae8
aliasif78 <aliasif1171@gmail.com>
2026-02-01 13:59:02 +0500

    feat: upsert user data into MongoDB after successful Google authentication callback.

--- app/login/google-callback/route.ts
+++ app/login/google-callback/route.ts
@@ -4,6 +4,10 @@ import { NextResponse } from 'next/server';
 // Supabase
 import { createClient } from '@/lib/supabase/server';
 
+// Database
+import { connectDB } from '@/database/mongoose';
+import User from '@/database/models/User';
+
 export async function GET(request: Request) {
   // URL
   // Will be something like: http://localhost:3000/login/google-callback?code=AbCdEf123456&next=/products
@@ -18,9 +22,39 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     // Success
-    if (!error) return NextResponse.redirect(`${origin}${next}`);
+    if (!error) {
+      // 1. Get the user details
+      const { data } = await supabase.auth.getUser();
+      const { user } = data;
+
+      // 2. User exists
+      if (user) {
+        // 3. Connect to the database
+        await connectDB();
+
+        // 4. Create or update user
+        const { email, user_metadata } = user;
+        const { full_name, avatar_url } = user_metadata;
+
+        // We use "findOneAndUpdate" with "upsert: true"
+        // Logic: Try to find the user. If found, update info. If NOT found, create them.
+        await User.findOneAndUpdate(
+          { email }, // Search by email
+          {
+            email,
+            name: full_name || email?.split('@')[0], // fallback to email prefix if name is missing
+            supabaseId: user.id,
+            profilePicture: avatar_url,
+          },
+          { upsert: true, new: true, setDefaultsOnInsert: true } // upsert: update or insert
+        );
+      }
+
+      // 5. Redirect to the next page
+      return NextResponse.redirect(`${origin}${next}`);
+    }
   }
 
-  // Error, redirect back to login
+  // 6. Error, redirect back to login
   return NextResponse.redirect(`${origin}/login?error=auth-code-error`);
 }
