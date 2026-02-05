5ca3736a6725839d52682b589e40fb20f54c4bc0
aliasif78 <aliasif1171@gmail.com>
2026-02-05 16:21:48 +0500

    feat: Differentiate Google login flow to sync status for returning users and set verified status for new users.

--- app/login/google-callback/route.ts
+++ app/login/google-callback/route.ts
@@ -8,6 +8,12 @@ import { createClient } from '@/lib/supabase/server';
 import { connectDB } from '@/database/mongoose';
 import User from '@/database/models/User';
 
+// BE Functions
+import { syncUserStatus } from '@/lib/users';
+
+// Constants
+import { VERIFIED } from '@/lib/globalConstants';
+
 export async function GET(request: Request) {
   // URL
   // Will be something like: http://localhost:3000/login/google-callback?code=AbCdEf123456&next=/products
@@ -36,18 +42,28 @@ export async function GET(request: Request) {
         const { email, user_metadata } = user;
         const { full_name, avatar_url } = user_metadata;
 
+        // 3. Check if user exists in Mongo
+        const mongoUser = await User.findOne({ email });
+
+        // --- SCENARIO A: RETURNING USER (Login) ---
+        if (mongoUser) await syncUserStatus(mongoUser._id, user.id);
+        // --- SCENARIO B: NEW USER (Sign Up) ---
+        // Create the user directly with "Verified" status
         // We use "findOneAndUpdate" with "upsert: true"
         // Logic: Try to find the user. If found, update info. If NOT found, create them.
-        await User.findOneAndUpdate(
-          { email }, // Search by email
-          {
-            email,
-            name: full_name || email?.split('@')[0], // fallback to email prefix if name is missing
-            supabaseId: user.id,
-            profilePicture: avatar_url,
-          },
-          { upsert: true, new: true, setDefaultsOnInsert: true } // upsert: update or insert
-        );
+        else
+          await User.findOneAndUpdate(
+            { email }, // Search by email
+            {
+              email,
+              name: full_name || email?.split('@')[0], // fallback to email prefix if name is missing
+              supabaseId: user.id,
+              status: VERIFIED, // Google users are always verified
+              profilePicture: avatar_url,
+              lastActive: new Date(),
+            },
+            { upsert: true, new: true, setDefaultsOnInsert: true } // upsert: update or insert
+          );
       }
 
       // 5. Redirect to the next page
