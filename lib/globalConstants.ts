5ba31c67dd120b6d40b4f62e4b920fa3ae49cb7e
aliasif78 <aliasif1171@gmail.com>
2026-02-06 22:04:01 +0500

    Added store model

--- lib/globalConstants.ts
+++ lib/globalConstants.ts
@@ -3,6 +3,14 @@ export const AMAZON = 'AMAZON';
 export const WOOCOMMERCE = 'WOOCOMMERCE';
 export const MANUAL = 'MANUAL';
 export const PLATFORMS = [SHOPIFY, AMAZON, WOOCOMMERCE, MANUAL];
+
+export enum EPlatform {
+  SHOPIFY = 'SHOPIFY',
+  AMAZON = 'AMAZON',
+  WOOCOMMERCE = 'WOOCOMMERCE',
+  MANUAL = 'MANUAL',
+}
+
 export const DEF_LOC_ID = 'WAREHOUSE_MAIN';
 
 // Role
