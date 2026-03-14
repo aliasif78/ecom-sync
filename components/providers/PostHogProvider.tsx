74fb1233b72d1b42d42fb352de8f7ec4ddec6839
aliasif78 <aliasif1171@gmail.com>
2026-03-14 15:29:11 +0500

    minor refactor

--- components/providers/PostHogProvider.tsx
+++ components/providers/PostHogProvider.tsx
@@ -1,17 +1,16 @@
 'use client';
 
-import posthog from 'posthog-js';
-import { PostHogProvider as PHProvider } from 'posthog-js/react';
+// React
 import { useEffect } from 'react';
 
+// PostHog
+import { register } from '@/instrumentation-client';
+import posthog from 'posthog-js';
+import { PostHogProvider as PHProvider } from 'posthog-js/react';
+
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-      person_profiles: 'identified_only',
-      capture_pageview: false, // We will handle this manually for better accuracy
-    });
-  }, []);
+  // Registering posthog client
+  useEffect(() => register(), []);
 
   return <PHProvider client={posthog}>{children}</PHProvider>;
 }
