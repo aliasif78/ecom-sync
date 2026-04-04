52fb06231fd4892364b2aa45c39f8537ddded450
aliasif78 <aliasif1171@gmail.com>
2026-04-04 21:55:11 +0500

    middleware inngest fix

--- proxy.ts
+++ proxy.ts
@@ -13,11 +13,12 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for:
+     * - api/inngest (webhook)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - images (public images)
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api/inngest|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
